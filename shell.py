231023c0bb4624631ef673122eb74f99defd3938
Lil-ie <lillywiens@proton.me>
2026-05-25 16:45:17 +0000

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,9 +1,58 @@
 '''----Pre recs----'''
 import time
-from account_setup import account_creation, login, user, password
 from filesystem import filesystem
 
 '''----Startup----'''
+# Pre recs
+account_created = False
+user = ''
+
+# Account functions
+def account_creation():
+    global user, account_created, password
+
+    if account_created == False:
+        while True:
+            if user == '':
+                user = input('Please enter a username: ')
+                continue
+            elif ' ' in user:
+                print('Username cannot have a space')
+                user = input('Please enter a username: ')
+                continue
+            else:
+                break
+        print('')
+        password = input(f'Please enter a strong password for {user}: ')
+        while True:
+            if password == "":
+                password = input(f'Please enter a strong password for {user}: ')
+                continue
+            else:
+                account_created = True
+                print('')
+                print('Account created successfully. Now sign in...')
+                print ('')
+                break
+
+def login():
+    global logged_in
+    if account_created == True:
+        command = input('Enter username: ')
+        while True:
+            if command == user:
+                command = input(f'Enter password for {user}: ')
+                if command == password:
+                    logged_in = True
+                    break
+                else:
+                    print(f'Incorrect password for: "{user}"')
+                    command = input(f'Enter password for {user}: ')
+            else:
+                print(f'Username: {command} not found')
+                command = input('Enter username: ')
+
+
 # Create account; *add login aswell*
 print('''
 Welcome to Coolinux :3
@@ -16,3 +65,5 @@ time.sleep(0.5)
 account_creation()
 time.sleep(0.5)
 login()
+
+
