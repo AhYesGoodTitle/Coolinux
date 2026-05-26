ff52b9abdcc1af4c0b503783b1ce689e74b8acb8
Lil-ie <lillywiens@proton.me>
2026-05-26 18:32:29 +0000

    Update filesystem.py

--- filesystem.py
+++ filesystem.py
@@ -1,3 +1,20 @@
 filesystem = {
-    "/": ['bin', 'etc', 'home']
+    '/': {
+        'bin': {
+            'readme.txt': ['Hello, World!']
+        },
+
+
+        'etc': {
+            'motd': ['''
+Welcome to Coolinux :3
+This is a unix inspired "VM" made in python!
+Check the git page for more technical information, disclaimers, and source code.
+https://github.com/Lil-ie/Coolinux
+
+You can edit the startup message in /etc/motd
+''']
+        }
+
+    }
 }
